c8a534abc4ab6811d79330f1b6299e1f766929ac
estebanmr99 <estebanmrj99@gmail.com>
2021-04-07 16:51:06 -0600

    More errors fixed - Linux

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { login, register, loginRequired } from '../controllers/userControllers.js';
+import { login, register, loginRequired } from '../Controllers/userControllers.js';
 import pg from 'pg';
 
 const config = {
